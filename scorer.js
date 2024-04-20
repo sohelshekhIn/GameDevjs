cafb4e6cc75a46a6fa4ea03b016b70f768c07fc8
masterha <masterha@sheridancollege.ca>
2024-04-20 14:02:48 -0400

    Brought everything in, PLS USE THIS BRANCH

---
+++ scorer.js
@@ -0,0 +1,10 @@
+import zim from "https://zimjs.org/cdn/016/zim_game";
+
+const scorer = (stage) =>{
+    var scorer = new Scorer({
+        borderColor:dark,
+    }).pos(30,30,RIGHT,BOTTOM)
+}
+
+export default scorer;
+
