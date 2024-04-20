0fd0dcda005934c3dbc49551b0d2342cf6dc461f
masterha <masterha@sheridancollege.ca>
2024-04-20 15:35:10 -0400

    checkpoint

--- girl.js
+++ girl.js
@@ -1,7 +1,7 @@
 import zim from "https://zimjs.org/cdn/016/zim_game";
 
 const girl = () =>{
-    var girl = new Pic("playdoll.png").sca(0.1).loc(0,70);
+    var girl = new Pic("playdoll.png").sca(0.1).loc(340,70);
 }
 
 export default girl;
--- timer.js
+++ timer.js
@@ -1,20 +1,27 @@
 import zim from "https://zimjs.org/cdn/016/zim_game";
 const clock = (stage) => {
   var timer = new Timer({
-    time: 60,
+    time: 1,
     // startPaused:true,
     // step:500,
     // colon:true,
     borderColor: dark,
   })
     .tap(function () {
-      timer.start(60);
+      timer.start(10);
       timer.backing.color = yellow;
     })
     .pos(30, 30, LEFT, BOTTOM); // .place();
   timer.on("complete", function () {
     timer.backing.color = red;
     timer.color = white;
+    // stage.update();
+    // console.log("Reached")
+
+    // var children = stage.children;
+    // console.log(S.children.Qo.Pic)
+    girl.x = 400; // Change the x-coordinate to the desired position
+    girl.y = 200; // Change the y-coordinate to the desired position
     stage.update();
   });
 };
